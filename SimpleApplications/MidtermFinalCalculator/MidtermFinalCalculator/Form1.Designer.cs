0335236d33313d6d7bd620638af1d06462ce8372
Berkay <61667359+BerkayDemiral@users.noreply.github.com>
2025-06-14 23:04:58 +0300

    Completed MidtermFinalCalculator_App

---
+++ SimpleApplications/MidtermFinalCalculator/MidtermFinalCalculator/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace MidtermFinalCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMidterm = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtBellCurve = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Midterm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Final";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            // 
+            // txtMidterm
+            // 
+            this.txtMidterm.Location = new System.Drawing.Point(100, 41);
+            this.txtMidterm.Name = "txtMidterm";
+            this.txtMidterm.Size = new System.Drawing.Size(298, 20);
+            this.txtMidterm.TabIndex = 3;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(100, 105);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(298, 20);
+            this.txtFinal.TabIndex = 4;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(100, 177);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(298, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(143, 223);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(206, 40);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(143, 294);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(206, 40);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtBellCurve
+            // 
+            this.txtBellCurve.Location = new System.Drawing.Point(100, 376);
+            this.txtBellCurve.Name = "txtBellCurve";
+            this.txtBellCurve.Size = new System.Drawing.Size(298, 20);
+            this.txtBellCurve.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(219, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Bell Curve";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 408);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBellCurve);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtMidterm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Midterm Final Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMidterm;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtBellCurve;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SimpleApplications/MidtermFinalCalculator/MidtermFinalCalculator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidtermFinalCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (txtMidterm.Text.Trim() == "" || txtFinal.Text.Trim() == "" || txtBellCurve.Text.Trim() == "")
+            {
+                MessageBox.Show("All Fields Must Be Filled Out!", "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                double midterm, final, result, bellCurve;
+                midterm = Convert.ToDouble(txtMidterm.Text);
+                final = Convert.ToDouble(txtFinal.Text);
+                bellCurve = Convert.ToDouble(txtBellCurve.Text);
+
+                if (midterm < 0 || midterm > 100 || final < 0 || final > 100 || bellCurve < 0 || bellCurve > 100)
+                {
+                    MessageBox.Show("Please Enter Values Between 0 and 100.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtResult.Clear();
+                    txtResult.BackColor = Color.White;
+                    return;
+                }
+
+                result = (midterm * 0.4) + (final * 0.6);
+                txtResult.Text = Math.Round(result, 2).ToString();
+
+                if (result >= bellCurve)
+                {
+                    txtResult.BackColor = Color.Green;
+                    txtResult.ForeColor = Color.White;
+                }
+                else
+                {
+                    txtResult.BackColor = Color.Red;
+                    txtResult.ForeColor = Color.White;
+                }
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please Enter Numbers Only!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMidterm.Clear();
+            txtFinal.Clear();
+            txtResult.Clear();
+            txtResult.BackColor = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtResult.ReadOnly = true;
+        }
+    }
+}
