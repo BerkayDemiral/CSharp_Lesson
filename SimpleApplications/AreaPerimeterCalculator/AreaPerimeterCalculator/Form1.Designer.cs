c0f31a45d2be153accb74d5c7c13538d71ff6bbe
Berkay <61667359+BerkayDemiral@users.noreply.github.com>
2025-06-15 02:34:16 +0300

    Completed AreaPerimeterCalculator_App

---
+++ SimpleApplications/AreaPerimeterCalculator/AreaPerimeterCalculator/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AreaPerimeterCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.rbArea = new System.Windows.Forms.RadioButton();
+            this.rbPerimeter = new System.Windows.Forms.RadioButton();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(99, 50);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 2;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(99, 103);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // rbArea
+            // 
+            this.rbArea.AutoSize = true;
+            this.rbArea.Location = new System.Drawing.Point(247, 50);
+            this.rbArea.Name = "rbArea";
+            this.rbArea.Size = new System.Drawing.Size(47, 17);
+            this.rbArea.TabIndex = 4;
+            this.rbArea.TabStop = true;
+            this.rbArea.Text = "Area";
+            this.rbArea.UseVisualStyleBackColor = true;
+            // 
+            // rbPerimeter
+            // 
+            this.rbPerimeter.AutoSize = true;
+            this.rbPerimeter.Location = new System.Drawing.Point(247, 104);
+            this.rbPerimeter.Name = "rbPerimeter";
+            this.rbPerimeter.Size = new System.Drawing.Size(69, 17);
+            this.rbPerimeter.TabIndex = 5;
+            this.rbPerimeter.TabStop = true;
+            this.rbPerimeter.Text = "Perimeter";
+            this.rbPerimeter.UseVisualStyleBackColor = true;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(99, 142);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculator_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(53, 185);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(40, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 223);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.rbPerimeter);
+            this.Controls.Add(this.rbArea);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Area Perimeter Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.RadioButton rbArea;
+        private System.Windows.Forms.RadioButton rbPerimeter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ SimpleApplications/AreaPerimeterCalculator/AreaPerimeterCalculator/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaPerimeterCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            int length, width, result;
+
+            if (!int.TryParse(txtLength.Text, out length) || length <= 0)
+            {
+                lblResult.Text = "Result: ";
+                MessageBox.Show("Please enter a valid positive number for length.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtLength.Focus();
+                return;
+            }
+
+            if (!int.TryParse(txtWidth.Text, out width) || width <= 0)
+            {
+                lblResult.Text = "Result: ";
+                MessageBox.Show("Please enter a valid positive number for width.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtWidth.Focus();
+                return;
+            }
+
+            if (rbArea.Checked)
+            {
+                result = length * width;
+                lblResult.Text = "Result: " + result.ToString();
+            }
+            else if (rbPerimeter.Checked)
+            {
+                result = 2 * (length + width);
+                lblResult.Text = "Result: " + result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please select an operation (Area or Perimeter).", "Selection Missing", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+    }
+}
