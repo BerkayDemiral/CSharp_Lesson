a5e88a4e41d5a9fbd0618ea011713074ef208897
Berkay <61667359+BerkayDemiral@users.noreply.github.com>
2025-06-16 23:52:21 +0300

    Completed PhoneDirectory_App

---
+++ PhoneDirectory/PhoneDirectory/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PhoneDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvContacts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(451, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(469, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(56, 22);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvContacts
+            // 
+            this.dgvContacts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContacts.Location = new System.Drawing.Point(13, 53);
+            this.dgvContacts.Name = "dgvContacts";
+            this.dgvContacts.Size = new System.Drawing.Size(450, 330);
+            this.dgvContacts.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(502, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(502, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(502, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Phone";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(565, 70);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(261, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(565, 104);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(261, 20);
+            this.txtLastname.TabIndex = 7;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(565, 141);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(261, 20);
+            this.txtPhone.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(565, 184);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(261, 25);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(565, 228);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(261, 25);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(565, 273);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(261, 25);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(562, 325);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(0, 13);
+            this.lblShow.TabIndex = 12;
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.BackColor = System.Drawing.Color.LightGreen;
+            this.btnExcel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnExcel.Location = new System.Drawing.Point(718, 12);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(108, 33);
+            this.btnExcel.TabIndex = 13;
+            this.btnExcel.Text = "Export to Excel";
+            this.btnExcel.UseVisualStyleBackColor = false;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 400);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvContacts);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "Form1";
+            this.Text = "Phone Directory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvContacts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblShow;
+        private System.Windows.Forms.Button btnExcel;
+    }
+}
+
---
+++ PhoneDirectory/PhoneDirectory/Form1.cs
@@ -0,0 +1,246 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataTable = System.Data.DataTable;
+
+namespace PhoneDirectory
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtPhone.KeyPress += TxtPhone_KeyPress;
+            txtPhone.MaxLength = 10;
+            dgvContacts.CellClick += dgvContacts_CellClick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadContacts();
+            CountContacts();
+        }
+
+        private void TxtPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private bool ValidateFields()
+        {
+            if (txtName.Text.Trim() == "" || txtLastname.Text.Trim() == "" || txtPhone.Text.Trim() == "")
+            {
+                MessageBox.Show("Please fill in all required fields.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (!long.TryParse(txtPhone.Text, out _) || txtPhone.Text.Length != 10)
+            {
+                MessageBox.Show("Please enter a valid numeric phone number without country code.", "Invalid Phone Number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void ClearFields()
+        {
+            txtName.Clear();
+            txtLastname.Clear();
+            txtPhone.Clear();
+            txtName.Focus();
+        }
+
+        private void LoadContacts()
+        {
+            using (var conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Initial Catalog=dbContact;Integrated Security=True"))
+            {
+                using (var adapter = new SqlDataAdapter("SELECT * FROM Contacts", conn))
+                {
+                    table = new DataTable();
+                    conn.Open();
+                    adapter.Fill(table);
+                    dgvContacts.DataSource = table;
+                }
+            }
+        }
+
+        private bool IsPhoneExists(string phone, int? idToExclude = null)
+        {
+            bool exists = false;
+            string query = "SELECT COUNT(*) FROM Contacts WHERE Phone = @Phone";
+
+            if (idToExclude.HasValue)
+                query += " AND Id <> @Id";
+
+            using (var conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Initial Catalog=dbContact;Integrated Security=True"))
+            using (var cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Phone", phone);
+                if (idToExclude.HasValue)
+                {
+                    cmd.Parameters.AddWithValue("@Id", idToExclude.Value);
+                }
+
+                conn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                exists = count > 0;
+            }
+
+            return exists;
+        }
+
+
+        private void CountContacts()
+        {
+            lblShow.Text = $"There are {dgvContacts.Rows.Count - 1} contacts in the phone directory";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!ValidateFields()) return;
+
+            if (IsPhoneExists(txtPhone.Text))
+            {
+                MessageBox.Show("This phone number already exists in the directory.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "INSERT INTO Contacts(Name, Lastname, Phone) VALUES (@Name, @Lastname, @Phone)";
+            using (conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Initial Catalog=dbContact;Integrated Security=True"))
+            using (cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Lastname", txtLastname.Text);
+                cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadContacts();
+            CountContacts();
+            MessageBox.Show($"{txtName.Text} {txtLastname.Text} has been added.", "Contact Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearFields();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (!ValidateFields()) return;
+
+            int currentId = (int)dgvContacts.CurrentRow.Cells[0].Value;
+
+            if (IsPhoneExists(txtPhone.Text, currentId))
+            {
+                MessageBox.Show("This phone number already exists in the directory.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "UPDATE Contacts SET Name = @Name, Lastname = @Lastname, Phone = @Phone WHERE Id = @Id";
+            using (conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Initial Catalog=dbContact;Integrated Security=True"))
+            using (cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Lastname", txtLastname.Text);
+                cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                cmd.Parameters.AddWithValue("@Id", dgvContacts.CurrentRow.Cells[0].Value);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadContacts();
+            CountContacts();
+            MessageBox.Show($"{txtName.Text} {txtLastname.Text} has been updated.", "Contact Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearFields();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvContacts.CurrentRow == null || dgvContacts.CurrentRow.Index < 0)
+            {
+                MessageBox.Show("Please select a contact first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string name = dgvContacts.CurrentRow.Cells[1].Value.ToString();
+            string lastname = dgvContacts.CurrentRow.Cells[2].Value.ToString();
+
+            string query = "DELETE FROM Contacts WHERE Id = @Id";
+            using (conn = new SqlConnection(@"Server=(localdb)\mssqllocaldb;Initial Catalog=dbContact;Integrated Security=True"))
+            using (cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@Id", dgvContacts.CurrentRow.Cells[0].Value);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadContacts();
+            CountContacts();
+            MessageBox.Show($"{name} {lastname} has been deleted.", "Contact Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearFields();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dv = table.DefaultView;
+                dv.RowFilter = $"Name LIKE '{txtSearch.Text}%'";
+                dgvContacts.DataSource = dv;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Contact not found.", "Search Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void dgvContacts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                txtName.Text = dgvContacts.Rows[e.RowIndex].Cells["Name"].Value.ToString().Trim();
+                txtLastname.Text = dgvContacts.Rows[e.RowIndex].Cells["Lastname"].Value.ToString().Trim();
+                txtPhone.Text = dgvContacts.Rows[e.RowIndex].Cells["Phone"].Value.ToString().Trim();
+            }
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Visible = true;
+            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+            int StartCol = 1;
+            int StartRow = 1;
+            for (int j = 0; j < dgvContacts.ColumnCount; j++)
+            {
+                Range myRange = (Range)sheet1.Cells[StartRow, StartCol + j];
+                myRange.Value2 = dgvContacts.Columns[j].HeaderText;
+            }
+            StartRow++;
+            for (int i = 0; i < dgvContacts.RowCount; i++)
+            {
+                for (int j = 0; j < dgvContacts.ColumnCount; j++)
+                {
+                    Range myRange = (Range)sheet1.Cells[StartRow + i, StartCol + j];
+                    myRange.Value2 = dgvContacts.Rows[i].Cells[j].Value;
+                    myRange.EntireColumn.AutoFit();
+                }
+            }
+        }
+    }
+}
